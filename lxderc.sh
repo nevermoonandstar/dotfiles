d43183cf8f5e81ff62c9714095321d2e68c44251
Eric Hoppe <hoppeeric@gmail.com>
2013-12-26 20:11:14 -0500

    Save my stuff to the cloud on startup

--- lxderc.sh
+++ lxderc.sh
@@ -5,3 +5,7 @@
 dropbox start #Start Dropbox
 gnome-terminal --window --full-screen #Terminal Screen @ Start
 xfce4-power-manager #Start power daemon, since Lubuntu doesn't
+#Move my org files into my Dropbox at startup
+find '/home/hoppe/org' '/home/hoppe/org_files' -type f | xargs mv -t /home/hoppe/Dropbox/org_files
+#Move any local documents into my Dropbox
+find '/home/hoppe/Documents' -type f | xargs mv -t /home/hoppe/Dropbox/personal_docs
